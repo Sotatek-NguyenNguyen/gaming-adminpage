1d07b522752a68047b37eca548afd26bf0a2f015
Sotatek-ThinhPhung2 <thinh.phung2@sotatek.com>
2021-12-17 10:26:39 +0700

    init homepage + 404 page + styling

--- components/Layouts/Header.js
+++ components/Layouts/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <nav className="header">
+            <h2 className="header__logo">Gamify</h2>
+        </nav>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
+import React from "react";
+import Header from "../components/Layouts/Header.js";
+import Button from "../components/UI/Button.js";
 
 function HomePage() {
-    return (
-        <div>
-            Home Page
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="container">
+        <div className="heading-primary">
+          <h2 className="heading-primary--main">Connect Your Wallet</h2>
+          <p className="heading-primary--sub">Connect your wallet to continue signing in</p>
+          <Button className="heading-primary--btn">Select Wallet</Button>
         </div>
-    )
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default HomePage;
