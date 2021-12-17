df8b9f3d0b0cbade99919ce5e64c4dd2e1fa733c
Sotatek-ThinhPhung2 <thinh.phung2@sotatek.com>
2021-12-17 16:34:09 +0700

    finish wallet list component

--- components/Layouts/Header.js
+++ components/Layouts/Header.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import gamifyLogo from "../../public/icons/gamify-text.svg";
 
 const Header = () => {
-    return (
-        <nav className="header">
-            <h2 className="header__logo">Gamify</h2>
-        </nav>
-    )
-}
+  return (
+    <nav className="header">
+        <img className="header__logo" src={gamifyLogo.src} />
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Layouts/Header.js";
 import Button from "../components/UI/Button.js";
 
 function HomePage() {
+  const [showWalletList, setShowWalletList] = useState(false);
+
+  const HeadingPrimary = () => {
+    return (
+      <div className="heading-primary">
+        <h2 className="heading-primary--main">Connect Your Wallet</h2>
+        <p className="heading-primary--sub">
+          Connect your wallet to continue signing in
+        </p>
+        <Button
+          onClick={() => setShowWalletList(true)}
+          className="heading-primary--btn"
+        >
+          Select Wallet
+        </Button>
+      </div>
+    );
+  };
+
+  const WalletItem = (props) => {
+    return (
+      <div className="wallet-item">
+        <div className="wallet-item__title">{props.name}</div>
+
+        <div className="wallet-item__logo">LOGO</div>
+      </div>
+    );
+  };
+
+  const WalletList = () => {
+    const walltets = [
+      { id: 0, name: "Phantom" },
+      { id: 1, name: "Sollet" },
+      { id: 2, name: "Sollet Extension" },
+    ];
+
+    return (
+      <div className="wallet-list">
+        <div className="wallet-list--heading">Connect a wallet</div>
+
+        <div className="wallet-list--main">
+          {walltets.map(w => (
+            <WalletItem key={w.id} name={w.name} />
+          ))}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <React.Fragment>
       <Header />
       <div className="container">
-        <div className="heading-primary">
-          <h2 className="heading-primary--main">Connect Your Wallet</h2>
-          <p className="heading-primary--sub">Connect your wallet to continue signing in</p>
-          <Button className="heading-primary--btn">Select Wallet</Button>
-        </div>
+        {!showWalletList ? <HeadingPrimary /> : <WalletList />}
       </div>
     </React.Fragment>
   );
