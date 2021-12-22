a53012e85aad36a6a0ac25ee122bc37d005921de
Dao Hoang Hiep <daohoanghiep2092000@gmail.com>
2021-12-23 00:01:29 +0700

    pull new code

--- components/Layouts/Topbar.js
+++ components/Layouts/Topbar.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import React from "react";
 import CurrentAccountBadge from "./CurrentAccountBadge ";
+import { Button } from '@mui/material';
 
 function Topbar() {
   const router = useRouter();
@@ -8,8 +9,20 @@ function Topbar() {
   const pageTitle = pathName[0].toUpperCase() + pathName.slice(1);
 
   return (
-    <div className="topbar">
-      <div className="topbar__title">{pageTitle}</div>
+    <div className="topbar container--custom">
+      <div className="topbar__title">
+        { pageTitle === 'Settings' ? 'Game Details' : pageTitle }
+      </div>
+      {
+        pageTitle === 'Player' 
+        ? 
+          <div className='players'>
+            <p className='players__total'>Total players: 0</p> 
+            <Button variant="contained" className='btn-main'>Export Player</Button>
+          </div>
+        : ''
+      }
+
       <CurrentAccountBadge />
     </div>
   );
--- pages/player/index.js
+++ pages/player/index.js
@@ -2,16 +2,33 @@ import React from 'react'
 import Layout from "../../components/Layouts/Layout";
 
 function PlayerPage() {
-    return (
-        <div>
-            Player Page 
+  return (
+    <div className="container--custom">
+      <section className='card-query'>
+        <h5 className='card__title'>Query</h5>
+        <div className='card__body'>
+           <div className='select--custom'>
+            <p>
+              <span>Most recent logins</span>
+              <span className='icon icon__dropdown'>dropdown</span>
+            </p>
+            <ul>
+              <li>-- Order result by --</li>
+              <li>Most recent logins</li>
+              <li>Highest value to date</li>
+            </ul>
+           </div>
         </div>
-    )
+      </section>
+
+
+    </div>
+  )
 }
 
 export default PlayerPage
 
 PlayerPage.getLayout = function getLayout(page) {
-    return <Layout>{page}</Layout>;
+  return <Layout>{page}</Layout>;
 };
   
