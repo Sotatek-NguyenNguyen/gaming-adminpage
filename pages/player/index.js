0f1fbb2874d79aa7ec8fde6207ed356267d52e51
Dao Hoang Hiep <daohoanghiep2092000@gmail.com>
2021-12-23 09:44:44 +0700

    player page, player detail

--- pages/player/index.js
+++ pages/player/index.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import Layout from "../../components/Layouts/Layout";
+import Dropdown from "../../components/UI/Dropdown";
+import { Button } from '@mui/material';
 
 function PlayerPage() {
+  const dropdownOptions = [
+    {title: 'Most recent logins', value: 0},
+    {title: 'Highest value to date', value: 1}
+  ];
+
   return (
-    <div className="container--custom">
-      <section className='card-query'>
+    <div className="container--custom players-contain">
+      <section className='card-custom'>
         <h5 className='card__title'>Query</h5>
-        <div className='card__body'>
-           <div className='select--custom'>
-            <p>
-              <span>Most recent logins</span>
-              <span className='icon icon__dropdown'>dropdown</span>
-            </p>
-            <ul>
-              <li>-- Order result by --</li>
-              <li>Most recent logins</li>
-              <li>Highest value to date</li>
-            </ul>
-           </div>
+        <div className='card__body card__query'>
+          <Dropdown options={dropdownOptions} className='query__dropdown' />
+          <input type="text" placeholder="Search players" />
+          <div className='card__interactive'>
+            <Button variant="outlined" className='btn-main--outline'>Clear</Button>
+            <Button variant="contained" className='btn-main'>Search</Button>
+          </div>
         </div>
       </section>
-
 
     </div>
   )
