8b489d4117f1891e6a8d988f9b8c96bc1f7b0358
Sotatek-ThinhPhung2 <thinh.phung2@sotatek.com>
2021-12-23 18:16:31 +0700

    feat: init catalog page

--- pages/catalog/index.js
+++ pages/catalog/index.js
@@ -1,8 +1,37 @@
 import React from "react";
 import Layout from "../../components/Layouts/Layout";
+import Input from "../../components/UI/Input.js";
+import Button from "../../components/UI/Button.js";
 
 function CatalogPage() {
-  return <div>Catalog Page</div>;
+  return (
+    <div className="catalog">
+      <div className="form-container">
+        <div className="form-container__header">
+          <h4>Game Balance</h4>
+        </div>
+        
+        <div className="form-container__content">
+          <form>
+            <label htmlFor="actual-game">
+              <h5>Actual game balance</h5>
+            </label>
+            <Input id="actual-game" />
+
+            <label htmlFor="in-game">
+              <h5>In-game balance</h5>
+            </label>
+            <Input id="in-game" />
+
+            <div className="form-actions">
+              <Button className='btn-main--outline'>Withdraw</Button>
+              <Button className='btn-main'>Deposit</Button>
+            </div>
+          </form>
+        </div>  
+      </div>
+    </div>
+  );
 }
 
 export default CatalogPage;
