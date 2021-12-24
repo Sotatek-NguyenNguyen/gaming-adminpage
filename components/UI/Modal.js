01e0de3df40cb79ac8cf270da064f357f648d41c
Sotatek-ThinhPhung2 <thinh.phung2@sotatek.com>
2021-12-24 17:07:21 +0700

    feat: modal basic structure

--- components/UI/Modal.js
+++ components/UI/Modal.js
@@ -1,30 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from "../UI/Button.js";
 
 const BackDrops = (props) => {
   return <div className="backdrop" onClick={props.onClick}></div>;
 };
 
-const ModalOverlay = () => {
-    return <div className="modal">
-        <div className="modal__header">
+const ModalOverlay = (props) => {
+  return (
+    <div className="modal">
+      <div className="modal__title">CONFIRM DEPOSIT TOKEN?</div>
 
+      <div className="modal__content">
+        <div className="address">
+          Destination Address <br />
+          4zj7KF13agrr3VYEt3RxxhDtzHGQmL7KdhzGZ9...
         </div>
+        <Input placeholder="Token Amount*" id="token-amount" />
+      </div>
 
-        <div className="modal__content">
-
-        </div>
-        
-        <div className="modal__actions">
-
-        </div>
+      <div className="modal__actions">
+        <Button className="btn-main--outline">Cancel</Button>
+        <Button className="btn-main">Confirm</Button>
+      </div>
     </div>
-}
+  );
+};
 
-function Modal() {
-  return <React.Fragment>
-      
-  </React.Fragment>;
+function Modal(props) {
+  const portalEl = document.getElementById("modal-root");
+
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<BackDrops />, portalEl)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalEl
+      )}
+    </React.Fragment>
+  );
 }
 
 export default Modal;
