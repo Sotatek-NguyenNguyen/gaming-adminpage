50a8f3d685fba92859916744e38f4982224f4ced
Sotatek-HiepDao <hiepdao@sotatek.com>
2021-12-26 16:50:28 +0700

    datatable component

--- pages/player/index.js
+++ pages/player/index.js
@@ -2,11 +2,91 @@ import React from 'react'
 import Layout from "../../components/Layouts/Layout";
 import Dropdown from "../../components/UI/Dropdown";
 import { Button } from '@mui/material';
+import DataTable from '../../components/UI/Table/DataTable';
 
 function PlayerPage() {
   const dropdownOptions = [
     {title: 'Most recent logins', value: 0},
     {title: 'Highest value to date', value: 1}
+  ];
+  const tableColumns = [
+    { title: 'User ID', field: 'id'},
+    { title: 'Username', field: 'name' },
+    { title: 'Age', field: 'age'},
+    { title: 'Email', field: 'email' },
+    { title: 'Country', field: 'country' }
+  ];
+  const tablesData = [
+    {
+      id: '2233',
+      name: 'test01',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    },
+    {
+      id: 'asdf',
+      name: 'test03',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    },
+    {
+      id: 'djf',
+      name: 'test04',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    },
+    {
+      id: 'afd',
+      name: 'test05',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    },
+    {
+      id: 'djf',
+      name: 'test04',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    },
+    {
+      id: 'afd',
+      name: 'test05',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    },
+    {
+      id: 'djf',
+      name: 'test04',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    },
+    {
+      id: 'afd',
+      name: 'test05',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    },
+    {
+      id: 'djf',
+      name: 'test04',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    },
+    {
+      id: 'afd',
+      name: 'test05',
+      age: 21,
+      email: 'test01@gmail.com',
+      country: 'VietNam'
+    }
   ];
 
   return (
@@ -22,6 +102,10 @@ function PlayerPage() {
           </div>
         </div>
       </section>
+
+      <div style={{marginTop: 8}}>
+        <DataTable columns={tableColumns} data={[]} tableMaxHeight={300} message="No transaction available"/>
+      </div>
 
     </div>
   )
