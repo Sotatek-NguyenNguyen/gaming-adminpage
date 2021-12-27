c6d7b5e26b864d33127028acf8444e09e18f19fa
Sotatek-ThinhPhung2 <thinh.phung2@sotatek.com>
2021-12-28 00:50:27 +0700

    feat: get transaction id

--- pages/overview/index.js
+++ pages/overview/index.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layouts/Layout";
 import Dropdown from "../../components/UI/Dropdown";
 import Card from "../../components/UI/Card.js";
+import { getJSON } from "../../common.js";
+import { ADMIN_PAGE_BACKEND_URL } from "../../config";
 
 function OverviewPage() {
+  const [transactionData, setTransactionData] = useState([]);
   const dropdownOptions = [
     { title: "Last 24 hours", value: 1 },
     { title: "Last 7 days", value: 7 },
     { title: "Last 30 days", value: 30 },
   ];
+
+  useEffect(() => {
+    const getTransactionData = async () => {
+      try {
+        const res = await getJSON(
+          `${ADMIN_PAGE_BACKEND_URL}/admin/users/transactions?page=1&pageSize=20`
+        );
+        if (res.status === 200) setTransactionData(res.data);
+      } catch (error) {
+        throw error;
+      }
+    };
+
+    getTransactionData().catch((err) => console.error(err.message));
+  }, []);
 
   function createData(name, amount, change) {
     return { name, amount, change };
@@ -37,9 +55,9 @@ function OverviewPage() {
       id: 3,
       title: "Reports",
       rows: [
-        createData("New Users", 3, "NA"),
+        createData("New Users", 0, "NA"),
         createData("Deposits", 5, 1),
-        createData("API Calls", 3, "NA"),
+        createData("Withdraws", 3, "NA"),
       ],
     },
   ];
@@ -50,14 +68,11 @@ function OverviewPage() {
         <span>Time period:</span>
         <Dropdown options={dropdownOptions} />
       </div>
-      
+
       <div className="card-list">
-       {
-         cardData.map(card => <Card 
-          key={card.id}
-          title={card.title}
-          rows={card.rows} />)
-       }
+        {cardData.map((card) => (
+          <Card key={card.id} title={card.title} rows={card.rows} />
+        ))}
       </div>
     </React.Fragment>
   );
